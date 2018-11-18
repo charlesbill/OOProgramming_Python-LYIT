50cd479d13803b3f6f294718437bc5f0a42a1eac
Charles Aylward <L00131535@student.lyit.ie>
2018-11-18 11:27:20 +0000

    Python 3 Variables Tutorial
    Answers for Python 3 Variables Tutorial

---
+++ Var_Question2.py
@@ -0,0 +1,22 @@
+one_Line = "^" * 50
+sides = "^"
+empty_Space = " " * 48
+mid_String: str = sides + empty_Space + sides
+the_Title = sides + " " * 21 + "Title" + " " * 22 + sides
+option_Word = "Option"
+option_A = "  A   Option a"
+option_B = "  B   Option b"
+option_C = "  C   Option c"
+print(one_Line)
+print(mid_String)
+print(mid_String)
+print(mid_String)
+print(the_Title)
+print(mid_String)
+print(mid_String)
+print(sides + option_A + " " * 34 + sides)
+print(sides + option_B + " " * 34 + sides)
+print(sides + option_C + " " * 34 + sides)
+print(mid_String)
+print(mid_String)
+print(one_Line)
